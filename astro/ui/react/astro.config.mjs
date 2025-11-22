a91342a986a64b58a99c3ae207fc345f8617ffa7
altie122 <git@altie122.xyz>
2025-11-21 18:31:54 -0700

    fixs

--- astro/ui/react/astro.config.mjs
+++ astro/ui/react/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [react()],
 });
